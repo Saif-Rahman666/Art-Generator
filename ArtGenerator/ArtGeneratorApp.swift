6efc251e6a2f52281cae006e4e448e6893a1d19a
Saif Rahman <Saf.saif@gmail.com>
2023-11-18 16:34:17 +0600

    Initial Commit

---
+++ ArtGenerator/ArtGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArtGeneratorApp.swift
+//  ArtGenerator
+//
+//  Created by Saif Rahman on 18/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct ArtGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
