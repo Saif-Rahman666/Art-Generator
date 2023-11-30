2cd4aabed577c8c92a9a1d7420c3dbf9bdd92518
Saif Rahman <Saf.saif@gmail.com>
2023-11-30 13:07:08 +0600

    initial

--- ArtGenerator/ContentView.swift
+++ ArtGenerator/ContentView.swift
@@ -4,13 +4,86 @@
 //
 //  Created by Saif Rahman on 18/11/2023.
 //
-
+import OpenAIKit
 import SwiftUI
 
+final class ViewModel: ObservableObject {
+    private var openai : OpenAI?
+    
+    func setup(){
+         openai = OpenAI(Configuration(
+            organization: "Personal",
+            apiKey: "API KEY"))
+    }
+    
+    func generateImage(prompt: String) async -> UIImage? {
+        guard let openai = openai else {
+            return nil
+        }
+        
+        do {
+            let params = ImageParameters(
+                prompt: prompt,
+                resolution: .medium,
+                responseFormat: .base64Json
+            )
+            let result = try await openai.createImage(parameters: params
+            )
+            let data = result.data[0].image
+            let image = try openai.decodeBase64Image(data)
+            return image
+        }
+        catch {
+            print(String(describing: error))
+            return nil
+        }
+
+    }
+}
+
 struct ContentView: View {
+    @ObservedObject var viewModel = ViewModel()
+    @State var text = ""
+    @State var image: UIImage?
+    
     var body: some View {
-        Text("Hello, world!")
+        NavigationView{
+            VStack{
+                Spacer()
+                if let image = image {
+                    Image(uiImage: image)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 250, height: 250)
+                }
+                else {
+                    Text("Type prompt to generate image!")
+                }
+                Spacer()
+                
+                TextField("Type prompt here..", text: $text)
+                    .padding()
+                Button("Generate!"){
+                    if !text.trimmingCharacters(in: .whitespaces).isEmpty{
+                        Task {
+                            let result = await viewModel.generateImage(prompt: text)
+                            if result == nil {
+                                print("Failed to get image")
+                            }
+                            self.image = result
+                        }
+                    }
+                    
+                   
+                }
+                
+            }
+            .navigationTitle("DALL-E Image Generator")
+            .onAppear{
+                viewModel.setup()
+            }
             .padding()
+        }
     }
 }
 
